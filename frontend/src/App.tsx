6449ad4d1f1c74ab2d4a3fa67e5a03229c5710bb
Rsengo <rsengo42@gmail.com>
2020-05-16 15:07:05 +0700

    atomic design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import BasePage from './components/templates/basePage/BasePage';
+import BaseTemplate from './templates/baseTemplate/BaseTemplate';
 import Navigation from './modules/navigation/Navigation';
 
 const App: React.FC = () => {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <BasePage
+    <BaseTemplate
       sideMenuOpened={open}
       onSideMenuOpen={() => setOpen(true)}
       onSideMenuClose={() => setOpen(false)}
     >
       <Navigation />
-    </BasePage>
+    </BaseTemplate>
   );
 };
 
